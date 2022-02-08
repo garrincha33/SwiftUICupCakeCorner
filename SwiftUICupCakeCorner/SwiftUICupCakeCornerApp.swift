2b52f92f84e2aa5b4c16a509de49dae43498dcca
Richard Price <richyprice77@gmail.com>
2022-02-08 14:29:36 +0000

    inital commit

---
+++ SwiftUICupCakeCorner/SwiftUICupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICupCakeCornerApp.swift
+//  SwiftUICupCakeCorner
+//
+//  Created by Richard Price on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
